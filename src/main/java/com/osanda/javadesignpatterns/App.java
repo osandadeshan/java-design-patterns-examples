6909e4ebc06b32d042aa9a9a8052af563d8b63ea
Osanda Deshan <osanda.deshan@gmail.com>
2020-10-11 10:32:07 +0530

    Initial commit

---
+++ src/main/java/com/osanda/javadesignpatterns/App.java
@@ -0,0 +1,13 @@
+package com.osanda.javadesignpatterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
